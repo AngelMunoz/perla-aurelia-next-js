3db6fd195966e8dbaa877361ab2be432d92a9d33
Angel D. Munoz <scythes.lol@gmail.com>
2021-12-28 17:37:11 -0600

    use hash router

--- src/main.js
+++ src/main.js
@@ -2,15 +2,6 @@ import Aurelia, { RouterConfiguration/*, StyleConfiguration*/ } from 'aurelia';
 import { MyApp } from './my-app.js';
 
 Aurelia
-  /*
-  .register(StyleConfiguration.shadowDOM({
-    // optionally add the shared styles for all components
-    sharedStyles: [shared]
-  }))
-  */
-  .register(RouterConfiguration)
-  // To use HTML5 pushState routes, replace previous line with the following
-  // customized router config.
-  // .register(RouterConfiguration.customize({ useUrlFragmentHash: false }))
+  .register(RouterConfiguration.customize({ useUrlFragmentHash: true }))
   .app(MyApp)
   .start();
